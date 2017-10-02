fbe6254ebe1a924a1dd190d2a1d85eabfa62f064
isaacyule <isaac.k.yule@gmail.com>
2017-10-02 11:53:39 +0100

    changed root to index

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,9 @@ Rails.application.routes.draw do
   get 'pages/index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get 'index' => 'pages#index'
   get 'welcome' => 'pages#home'
   get 'about' => 'pages#about'
+
+  root 'pages#index'
   
 end
