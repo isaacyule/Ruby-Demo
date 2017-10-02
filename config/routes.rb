f982372346549195f7b39c07a66066e03c52d5d9
isaacyule <isaac.k.yule@gmail.com>
2017-10-02 12:00:39 +0100

    updated root to Homepage, deleted index/welcome page

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,9 @@
 Rails.application.routes.draw do
   get 'pages/about'
 
-  get 'pages/index'
-
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get 'welcome' => 'pages#home'
   get 'about' => 'pages#about'
 
-  root 'pages#index'
+  root 'pages#home'
   
 end
